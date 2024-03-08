bffa13cfd9ad811d22540334ec245715a0058ee0
Jungmin Moon <jmmoon96@gmail.com>
2024-03-07 19:50:27 -0500

    Creating some initial files and folders for the project

---
+++ module-info.java
@@ -0,0 +1,6 @@
+module gui.database {
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens projectgui;
+}
---
+++ projectgui/Scene_Repository.java
@@ -0,0 +1,13 @@
+package projectgui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+
+//will be using border pane for the various scenes to organize them better
+
+public class Scene_Repository {
+}
